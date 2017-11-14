260c54b384c509171497dff64f29906355f722b4
andres velasco <andyvemh@hotmail.com>
2017-11-14 11:40:58 -0500

    Generador de numeros randomicos

---
+++ pseudoRand.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+long int mod(long int a,long int b){
+	return a%b;
+}
+
+int main(int argc, char *argv[]){
+ 	  long int n = atoi (argv[1]);
+    long int s = atoi (argv[2]);
+    long int x = mod(pow(s,2),n);
+		int i=0;
+		int b;
+		printf("%i          %i          %i\n",i,x,b);
+		
+ 		for (i=1;i<21;i++){
+				x=mod(pow(x,2),n);
+				b=mod(x,2);
+				printf("%i          %i          %i\n",i,x,b);
+		}
+}
